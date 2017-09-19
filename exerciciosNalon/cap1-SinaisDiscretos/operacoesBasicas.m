415ef7e76da5f14f31f72ca8b44841a91c314e72
miguel91it <miguel91_it@hotmail.com>
2017-09-19 16:47:05 -0300

    commit

---
+++ exerciciosNalon/cap1-SinaisDiscretos/operacoesBasicas.m
@@ -0,0 +1,55 @@
+%operacoes basicas com sinais
+%soma
+%subtracao
+%multiplicacao
+%divisao
+
+n = 0:1:100;
+
+x1 = sin(n);
+
+x2 = 0*n + 1;
+
+sum = x1+x2;
+
+diff = x1 - x2;
+
+mult = x1 .* x2;
+
+div = x1 ./ x2;
+
+p = plot(n, x1, n, x2, n, sum);
+title('sum x1[n] + x2[n]')
+xlabel('n')
+ylabel('x[n]')
+p(1).Color = 'red';
+p(2).Color = 'blue';
+p(3).Color = 'black';
+p(3).LineStyle = '--';
+
+figure, p = plot(n, x1, n, x2, n, diff);
+title('diff x1[n] - x2[n]')
+xlabel('n')
+ylabel('x[n]')
+p(1).Color = 'red';
+p(2).Color = 'blue';
+p(3).Color = 'black';
+p(3).LineStyle = '--';
+
+figure, p = plot(n, x1, n, x2, n, mult);
+title('mult x1[n] * x2[n]')
+xlabel('n')
+ylabel('x[n]')
+p(1).Color = 'red';
+p(2).Color = 'blue';
+p(3).Color = 'black';
+p(3).LineStyle = '--';
+
+figure, p = plot(n, x1, n, x2, n, div);
+title('div x1[n] / x2[n]')
+xlabel('n')
+ylabel('x[n]')
+p(1).Color = 'red';
+p(2).Color = 'blue';
+p(3).Color = 'black';
+p(3).LineStyle = '--';
